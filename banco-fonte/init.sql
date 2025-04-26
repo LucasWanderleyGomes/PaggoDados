4012b68e45ddf7363015d3b6072835761d9ecaa6
Lucas Wanderley Gomes <lucas.wanderley.gomes@gmail.com>
2025-04-26 18:42:58 -0300

    Reinicio

--- banco-fonte/init.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS data (
-    timestamp TIMESTAMP NOT NULL,
-    wind_speed FLOAT,
-    power FLOAT,
-    ambient_temperature FLOAT
-);
--- dagster/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import assets
-from . import resources
